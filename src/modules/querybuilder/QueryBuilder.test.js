5000af3824353859e4008363e42c34c8e914a806
Jamie Boyd <enkizero@gmail.com>
2018-05-10 17:22:20 +0100

    Added a toContain test to QueryBuilder module

--- src/modules/querybuilder/QueryBuilder.test.js
+++ src/modules/querybuilder/QueryBuilder.test.js
@@ -4,6 +4,10 @@ it('returns a query string', () => {
   expect(buildGithubAPIQuery('username')).toBeTruthy();
 });
 
+it('includes the passed username in the query string', () => {
+  expect(buildGithubAPIQuery('shadwell')).toContain('shadwell');
+});
+
 it('builds the correct query', () => {
   const queryMatch = 'http://api.github.com/users/jamboid/repos';
   expect(buildGithubAPIQuery('jamboid')).toBe(queryMatch);
