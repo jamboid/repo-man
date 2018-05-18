75a40742f5cf90af0cba8092c92ba5ce5f679218
Jamie Boyd <enkizero@gmail.com>
2018-05-18 16:04:22 +0100

    Simplified Logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from "./repo_man_logo.svg";
 import './Logo.css';
 
-class Logo extends Component {
-
-  render() {
-    return (
-      <h1 className="cp_Logo">
-        <img className="cp_Logo__image" src={logo} alt="Repo Man"/>
-      </h1>
-    );
-  }
-}
+const Logo = () => (
+  <h1 className="cp_Logo">
+    <img className="cp_Logo__image" src={logo} alt="Repo Man"/>
+  </h1>
+);
 
 export default Logo;
