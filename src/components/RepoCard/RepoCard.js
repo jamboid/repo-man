6d1eaaf60e513ec4b4fdfea4d2723670e86e3c2b
Jamie Boyd <jamboid@users.noreply.github.com>
2018-05-28 23:51:11 +0100

    RepoMan: updated card layout

--- src/components/RepoCard/RepoCard.js
+++ src/components/RepoCard/RepoCard.js
@@ -6,9 +6,12 @@ import './RepoCard.css';
 function RepoCard(props) {
   return (
     <div className="cp_RepoCard">
-      <h2 className="cp_RepoCard__title">{props.name}</h2>
-      <p className="cp_RepoCard__desc">{props.description}</p>
-      <a className="cp_RepoCard__link" href={props.url} target="_blank">View repository</a>
+      <div className="cp_RepoCard__details">
+        <h2 className="cp_RepoCard__title">{props.name}</h2>
+        <p className="cp_RepoCard__desc">{props.description}</p>
+      </div>
+        <a className="cp_RepoCard__link" href={props.url} target="_blank" title="View this repo">View</a>
+
     </div>
   );
 }
