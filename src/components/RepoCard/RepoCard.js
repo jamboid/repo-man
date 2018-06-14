5a2838ce971a42d934a9970ecb7f7abf6e00c414
Jamie Boyd <enkizero@gmail.com>
2018-06-14 08:56:39 +0100

    Inlined SVG icons and logos

--- src/components/RepoCard/RepoCard.js
+++ src/components/RepoCard/RepoCard.js
@@ -13,11 +13,13 @@ function RepoCard(props) {
         <p className="cp_RepoCard__desc">{props.description}</p>
       </div>
       <div className="cp_RepoCard__stars">
-        <img className="cp_RepoCard__starsIcon" src={star} />
+        <svg className="cp_RepoCard__starsIcon"  width="23" height="22" xmlns="http://www.w3.org/2000/svg"><path d="M11.5 16.937L4.393 22l2.591-8.363L0 8.403l8.709-.105L11.5 0l2.791 8.298L23 8.403l-6.984 5.234L18.607 22z" fill="#FFCF00" fill-rule="evenodd"/></svg>
         <p className="cp_RepoCard__starsTotal">{props.stars}</p>
       </div>
       <a className="cp_RepoCard__link" href={props.url} target="_blank">
-        <img className="cp_RepoCard__linkIcon" src={arrow} />
+        <svg className="cp_RepoCard__linkIcon" width="36" height="35" xmlns="http://www.w3.org/2000/svg"><g stroke="#FFF" stroke-width="5" fill="none" fill-rule="evenodd" stroke-linecap="round"><path stroke-linejoin="round" d="M17.718 2.567L32.542 17.39 17.719 32.213"/><path d="M29.916 17.567H2.659"/></g></svg>
+
+
       </a>
 
     </div>
